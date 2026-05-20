9c8788f8aeeead70dfffdff38f83eba381c5b215
haleysc0met <haleyannecc@gmail.com>
2026-05-20 10:17:02 -0400

    changed id names

---
+++ script.js
@@ -0,0 +1,6 @@
+const container1 = document.getElementById("container");
+const container2 = document.getElementById("container2");
+const container3 = document.getElementById("container3");
+const slider1 = document.getElementById("slider1");
+const slider2 = document.getElementById("slider2");
+const slider3 = document.getElementById("slider3");
