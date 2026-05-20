e50d83d5cc9d7a966d5194236c35fa3dfc3f52d9
haleysc0met <haleyannecc@gmail.com>
2026-05-20 10:45:36 -0400

    troubleshooting and fixed flex display

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const container3 = document.getElementById("container3");
 const slider1 = document.getElementById("slider1");
 const slider2 = document.getElementById("slider2");
 const slider3 = document.getElementById("slider3");
+
+// Slider 1 hiding when user interacts with it -> slider 2 appears
+slider1.addEventListener("input", () => {
+    container1.classList.add("hidden");
+    container2.classList.remove("hidden");
+});
+
+// Slider 2 hiding when user interacts with it -> slider 3 appears
+slider2.addEventListener("input", () => {
+    // resetting text before switching sliders
+    Text.style.transform = 'translateX(0px) rotate(0deg)';
+    container2.classList.add("hidden");
+    container3.classList.remove("hidden");
+});
+
+//slider 3 actually adjusts text size
+slider3.addEventListener("input", (e) => {
+    const val = e.target.value;
+    Text.style.fontSize = val + "px";
+});
